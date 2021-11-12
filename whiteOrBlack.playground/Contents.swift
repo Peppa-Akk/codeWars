a87d43ffbf9c4efddfd0dba352e3c8de6ab1394e
Alexander <hasman32320@gmail.com>
2021-11-12 16:57:03 +0300

    + whiteOrBlack (edit)

--- whiteOrBlack.playground/Contents.swift
+++ whiteOrBlack.playground/Contents.swift
@@ -2,17 +2,9 @@ let letterInNumber = ["a": 1, "b": 2, "c": 3, "d": 4,
                       "e": 5, "f": 6, "g": 7, "h": 8]
 
 func isBlack(first: String, second: Int) -> String {
-    var counter = 0
     
-    if letterInNumber[first]! % 2 == 0 {
-        counter += 1
-    }
-    
-    if second % 2 != 0 {
-        counter += 1
-    }
-    
-    return counter % 2 == 0 ? "\(first).\(second) White" : "\(first).\(second) Black"
+    return (letterInNumber[first]! + second) % 2 == 0 ? "\(first).\(second) Black" :
+        "\(first).\(second) White"
 }
 
 var chessLetterRoad = ["a", "b", "c", "d",
